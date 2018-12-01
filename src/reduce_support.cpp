a9a224b7ae2867a5ce1d9c52319b243794bc9f91
eriszhang <jiayieris.zhang@mail.utoronto.ca>
2018-11-30 20:42:58 -0500

    what happened to fit_rotation

--- src/reduce_support.cpp
+++ src/reduce_support.cpp
@@ -192,9 +192,15 @@ float reduce_support(
 
         double E_arap, E_overhang, E_intersect;
 
+
         E_overhang = overhang_energy(U, F, bnd, config.dp, tau, config.is3d, unsafe);
-        E_arap = arap_energy(V, T, M, F, L, K);
+
+
+        E_arap = arap_energy(V, T, M, config.is3d?Tet:F, L, K, config.is3d);
+
+
         E_intersect = 0;
+
 
         iter += 1;
         float fX = (float) (
@@ -202,6 +208,7 @@ float reduce_support(
             config.c_overhang * E_overhang + 
             config.c_intersect * E_intersect
         );
+
 
         std::cout<<"["<<iter<<"] f(X): "<<fX<<
             "\t\t("<<config.c_arap*E_arap<<", "<<
